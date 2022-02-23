afe544e2bed3434a18dbfc1aca4fa765c25bfaf5
unknown <Ingrid Silva>
2022-02-23 16:37:37 -0300

    implements type to val

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,5 +1,15 @@
 fun main() {
     println("Bem vindo ao ByteBank")
-    val titular = "Alex"
+    val titular: String = "Alex"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+
+    saldo = 100 + 2.0
+    saldo += 200
+
     println("titular $titular")
+    println("numero da conta $numeroConta")
+    println("saldo da conta $saldo")
+
+
 }
