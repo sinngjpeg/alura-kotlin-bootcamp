db796a48cc0dccc0c836fb5dd1d5392605ea3f9b
unknown <Ingrid Silva>
2022-03-02 23:18:49 -0300

    implements for and while

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,16 +1,22 @@
 fun main() {
     println("Bem vindo ao ByteBank")
-    val titular: String = "Alex"
-    val numeroConta: Int = 1000
-    var saldo: Double = 0.0
 
-    saldo = 100 + 2.0
-    saldo += 200
+}
 
-    println("titular $titular")
-    println("numero da conta $numeroConta")
-    println("saldo da conta $saldo")
+fun testaLaco() {
+    var i = 0
+    while (i < 5) {
+        val titular: String = "Alex $i"
+        val numeroConta: Int = 1000 + i
+        var saldo: Double = i + 10.0
 
+        println("titular $titular")
+        println("numero da conta $numeroConta")
+        println("saldo da conta $saldo")
+    }
+}
+
+fun testaCondicoes(saldo: Double) {
     when {
         saldo > 0.0 -> println("conta positiva")
         saldo == 0.0 -> println("conta neutra")
